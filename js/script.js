f6418e994a2679903600c5913d6204ccd34ae545
arkon <ey.cheung@outlook.com>
2014-11-09 16:46:29 -0500

    Better JS code/factoring

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// For showing/hiding elements
-Element.prototype.show = function() {
-  this.style.display = 'block';
-};
-
-Element.prototype.hide = function() {
-  this.style.display = 'none';
-};
-
-
 // The views
 var view1 = document.getElementById('view-1'),
     view2 = document.getElementById('view-2'),
@@ -16,39 +6,18 @@ var view1 = document.getElementById('view-1'),
     view5 = document.getElementById('view-5'),
     view6 = document.getElementById('view-6');
 
-// Hide all views
-function hideViews() {
-  view1.hide();
-  view2.hide();
-  view3.hide();
-  view4.hide();
-  view5.hide();
-  view6.hide();
-}
-
-Element.prototype.showView = function(view) {
-  this.onclick = function() {
-    hideViews();
-    view.show();
-  }
-};
-
 
 // Initial view
 view1.show();
 
-
 // View 1 (main)
 document.getElementById('view-1-add').showView(view2);
-
 document.getElementById('view-1-reminders').showView(view6);
 
 
 // View 2 (add)
 document.getElementById('view-2-add-blood').showView(view3);
-
 document.getElementById('view-2-add-food').showView(view5);
-
 document.getElementById('view-2-add-exercise').showView(view4);
 
 
