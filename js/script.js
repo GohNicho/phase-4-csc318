5f5b370dda144f7c51adee516a3aaf982170e0b4
arkon <ey.cheung@outlook.com>
2014-11-09 17:21:51 -0500

    Some home screen stuff

--- js/script.js
+++ js/script.js
@@ -1,34 +1,18 @@
-// The views
-var view1 = document.getElementById('view-1'),
-    view2 = document.getElementById('view-2'),
-    view3 = document.getElementById('view-3'),
-    view4 = document.getElementById('view-4'),
-    view5 = document.getElementById('view-5'),
-    view6 = document.getElementById('view-6');
-
+// Home page
+var vHome = document.getElementById('home'),
+    vHomeEstimate = document.getElementById('home-estimate'),
+    vHomeAdd = document.getElementById('home-add'),
+    vHomeInstructions = document.getElementById('home-instructions'),
+    vHomeMenu = document.getElementById('home-menu'),
+    vHomeTrends = document.getElementById('home-trends');
 
 // Initial view
-view1.show();
+vHome.show();
 
-// View 1 (main)
-document.getElementById('view-1-add').showView(view2);
-document.getElementById('view-1-reminders').showView(view6);
-
-
-// View 2 (add)
-document.getElementById('view-2-add-blood').showView(view3);
-document.getElementById('view-2-add-food').showView(view5);
-document.getElementById('view-2-add-exercise').showView(view4);
-
-
-// View 3-5 (input data)
-document.getElementById('view-3-cancel').showView(view1);
-document.getElementById('view-3-done').showView(view1);
-document.getElementById('view-4-cancel').showView(view1);
-document.getElementById('view-4-done').showView(view1);
-document.getElementById('view-5-cancel').showView(view1);
-document.getElementById('view-5-done').showView(view1);
-
-
-// View 6 (reminders)
-document.getElementById('view-6-glucose').showView(view1);
+document.getElementById('home-estimate-toggle').showView(vHomeEstimate);
+document.getElementById('home-add-button').showView(vHomeAdd);
+document.getElementById('home-instructions-button').showView(vHomeInstructions);
+document.getElementById('home-menu-button').showView(vHomeMenu);
+document.getElementById('home-trends-button').showView(vHomeTrends);
+document.getElementById('home-trends-button').showView(vHomeTrends);
+document.getElementById('home-instructions-close').showView(vHome);
