2df3408cdbd4e475d16d0076ef3daddf5d2fc2c4
arkon <ey.cheung@outlook.com>
2014-11-09 20:58:42 -0500

    Reminders tweak

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ document.getId('home-trends-close').showView(vHome);
 
 // Home menu
 document.getId('home-menu-settings').showView(document.getId('settings'))
-document.getId('home-menu-reminders').showView(document.getId('reminders'))
+document.getId('home-menu-reminders').showView(document.getId('reminders-day'))
 document.getId('home-menu-passcode').showView(document.getId('passcode'))
 document.getId('home-menu-cancel').showView(vHome)
 
@@ -86,10 +86,22 @@ document.getId('input-exercise-running').showView(document.getId('input-exercise
 
 // Reminders
 var vReminderDetails = document.getId('reminder-details');
+
+document.getId('reminders-day-back').showView(vHome);
+document.getId('reminders-month-tab').showView(document.getId('reminders-month'));
+document.getId('reminder-edit').showView(vReminderDetails);
+document.getId('reminders-day-add').showView(document.getId('reminders-add'));
+
+document.getId('reminders-month-back').showView(vHome);
+document.getId('reminders-day-tab').showView(document.getId('reminders-day'));
+document.getId('reminders-month-add').showView(document.getId('reminders-add'));
+
+document.getId('reminders-add-back').showView(document.getId('reminders-day'));
+
 document.getId('reminders-back').showView(vHome);
 document.getId('reminder-example').showView(vReminderDetails);
 
-document.getId('reminder-details-back').showView(document.getId('reminders'));
+document.getId('reminder-details-back').showView(document.getId('reminders-day'));
 document.getId('reminder-details-settings').showView(document.getId('reminder-details-repeat'));
 
 document.getId('reminder-details-repeat-close').showView(vReminderDetails);
