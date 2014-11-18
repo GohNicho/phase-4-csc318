28a05b3c828fe31cd120faaf86469d50bb0f80d3
Eugene Cheung <ey.cheung@outlook.com>
2014-11-18 15:21:53 -0500

    Remove estimate and trends menu

--- js/script.js
+++ js/script.js
@@ -24,23 +24,17 @@ document.getId('tutorial-reminders-next').showView(document.getId('tutorial-prog
 document.getId('tutorial-progress-next').showView(vHome);
 
 // Home page
-document.getId('home-estimate-toggle').showView(document.getId('home-estimate'));
-document.getId('home-estimate-back').showView(vHome);
 document.getId('home-add-button').showView(document.getId('home-add'));
 document.getId('home-instructions-button').showView(document.getId('home-instructions'));
 document.getId('home-instructions-close').showView(vHome);
 document.getId('home-menu-button').showView(document.getId('home-menu'));
-document.getId('home-trends-button').showView(document.getId('home-trends'));
+document.getId('home-trends-button').showView(document.getId('glucose-trends'));
 
 // Home add menu
 document.getId('home-add-close').showView(vHome);
 document.getId('home-add-glucose').showView(document.getId('input-glucose'));
 document.getId('home-add-meal').showView(document.getId('input-food'));
 document.getId('home-add-exercise').showView(document.getId('input-exercise'));
-
-// Home trends menu
-document.getId('home-trends-close').showView(vHome);
-document.getId('home-trends-glucose').showView(document.getId('glucose-trends'));
 
 // Home menu
 document.getId('home-menu-settings').showView(document.getId('settings'))
@@ -77,13 +71,11 @@ document.getId('input-food-date-button').showView(document.getId('input-food-dat
 document.getId('input-food-time-done').showView(document.getId('input-food'));
 document.getId('input-food-date-done').showView(document.getId('input-food'));
 
-
 document.getId('input-exercise-back').showView(vHome);
 document.getId('input-exercise-time-button').showView(document.getId('input-exercise-time'));
 document.getId('input-exercise-date-button').showView(document.getId('input-exercise-date'));
 document.getId('input-exercise-time-done').showView(document.getId('input-exercise'));
 document.getId('input-exercise-date-done').showView(document.getId('input-exercise'));
-
 
 document.getId('input-exercise-options-dropdown').showView(document.getId('input-exercise-options'))
 document.getId('input-exercise-running').showView(document.getId('input-exercise'))
